0eda87ab086bc903939489e96e7e509223dbc90c
KDirga <keanna.dirga@uri.edu>
2026-03-19 09:26:51 -0400

    Part II Display The First Toy

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php 
 
-    /* TO-DO: Include header.php
-              Hint: header.php is inside the includes folder and already connects to the database
-    */
 	include 'includes/header.php';
 
 
@@ -10,7 +7,7 @@
     /*
 	 * Retrieve toy information from the database based on the toy ID.
 	 * 
-	 * @param PDO $pdo       An instance of the PDO class.
+	 * @param PDO $pdo       An instance of the PDO class. (PDO = connection between database and PHP)
 	 * @param string $id     The ID of the toy to retrieve.
 	 * @return array|null    An associative array containing the toy information, or null if no toy is found.
 	 */
@@ -38,20 +35,21 @@
     <div class="toy-card">
   	    <!-- TO-DO: Create a hyperlink to toy.php and pass the toy number as a URL parameter
                     Hint: Access the value from the $toy1 array (what is the column name in the database?) -->
-  	    <a href="toy.php?toynum=<?= '' ?>">
+  	    <a href="toy.php?toynum=<?= $toy1['toyID'] ?>">
+			
 
   		    <!-- TO-DO: Display the toy image and update the alt text to the toy name
                         Hint: Access the values from the $toy1 array (what are the column names in the database?) -->
-  			<img src="<?= '' ?>" alt="<?= '' ?>">
+  			<img src="<?= $toy1['imagePath'] ?>" alt="<?= $toy1['name'] ?>">
   		</a>
 
   		<!-- TO-DO: Display the name of the toy
                     Hint: Access the value from the $toy1 array (what is the column name in the database?) -->
-  		<h2><?= '' ?></h2>
+  		<h2><?= $toy1['name'] ?></h2>
 
   		<!-- TO-DO: Display price of toy 
                     Hint: Access the value from the $toy1 array (what is the column name in the database?) -->
-  		<p>$<?= '' ?></p>
+  		<p>$<?= $toy1['price'] ?></p>
   	</div>
     <!-- TOY CARD END -->
 
